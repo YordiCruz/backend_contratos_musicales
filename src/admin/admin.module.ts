5575f3a8710c952498a4281147db03645c14b2cc
Yordi <yordi.cruz@gmail.com>
2026-02-24 14:02:13 -0400

    creado de modulos para la gestion de eventos

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -11,6 +11,7 @@ import { EspecialidadesModule } from './especialidades/especialidades.module';
 import { ReemplazosModule } from './reemplazos/reemplazos.module';
 import { IntegrantesModule } from './integrantes/integrantes.module';
 import { CategoriasEspecialidadsModule } from './especialidades/categorias_especialidads/categorias_especialidads.module';
+import { EventosModule } from './eventos/eventos.module';
 
 @Module({
     imports: [
@@ -21,7 +22,8 @@ import { CategoriasEspecialidadsModule } from './especialidades/categorias_espec
     EspecialidadesModule,
     ReemplazosModule,
     IntegrantesModule,
-    CategoriasEspecialidadsModule
+    CategoriasEspecialidadsModule,
+    EventosModule
 ],
 
 
---
+++ src/admin/eventos/eventos.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { CategoriasModule } from './categorias/categorias.module';
+import { EventosModule } from './eventos/eventos.module';
+import { MediaModule } from './media/media.module';
+
+@Module({
+  imports: [CategoriasModule, EventosModule, MediaModule]
+})
+export class EventosModule {}
