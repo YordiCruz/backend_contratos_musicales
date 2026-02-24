6dafd435739c3ce88940c55f1767a2ca85301efd
Yordi <yordi.cruz@gmail.com>
2026-02-24 13:11:18 -0400

    service y controler de integrantes para añadir especialidades

--- src/admin/integrantes/integrantes.controller.ts
+++ src/admin/integrantes/integrantes.controller.ts
@@ -3,6 +3,8 @@ import { IntegrantesService } from './integrantes.service';
 import { CreateIntegranteDto } from './dto/create-integrante.dto';
 import { UpdateIntegranteDto } from './dto/update-integrante.dto';
 import { FiltroIntegranteDataDto } from './dto/filtro-integrante-data.dto';
+import { AsignarEspecialidadDto } from './dto/asignar-especialidad.dto';
+import { AsignarVariasEspecialidadesDto } from './dto/asignar-varias-especialidades.dto';
 
 @Controller('integrantes')
 export class IntegrantesController {
@@ -38,5 +40,37 @@ export class IntegrantesController {
   createIntegranteFromExistingPersona(@Param('idPersona') idPersona: string,@Param() req , @Body() dto: any) {
     return this.integrantesService.createIntegranteFromExistingPersona(idPersona, dto, req.user);
   }
+
+  @Get(':id/especialidades')
+  listEspecialidades(@Param('id') id: string) {
+    return this.integrantesService.listaEspecialidades(id);
+  }
+
+  @Post(':id/especialidades')
+  asignarEspecialidad(
+    @Param('id') id: string,
+    @Body() dto: AsignarEspecialidadDto,
+  ) {
+    return this.integrantesService.asignarEspecialidad(id, dto);
+  }
+
+  @Post(':id/especialidades/multiples')
+  asignarMultiples(
+    @Param('id') id: string,
+    @Body() dto: AsignarVariasEspecialidadesDto,
+  ) {
+    return this.integrantesService.asignarMultiplesEspecialidades(id, dto);
+  }
+
+  @Delete(':id/especialidades/:idEspecialidad')
+  eliminarEspecialidad(
+    @Param('id') id: string,
+    @Param('idEspecialidad') idEspecialidad: string,
+  ) {
+    return this.integrantesService.eliminarEspecialidad(id, idEspecialidad);
+  }
+
+
+
 
 }
--- src/admin/integrantes/integrantes.module.ts
+++ src/admin/integrantes/integrantes.module.ts
@@ -4,9 +4,10 @@ import { IntegrantesController } from './integrantes.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Integrante } from './entities/integrante.entity';
 import { Persona } from '../personas/entities/persona.entity';
+import { Especialidad } from '../especialidades/especialidads/entities/especialidad.entity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Integrante])],
+  imports:[TypeOrmModule.forFeature([Integrante, Especialidad])],
   controllers: [IntegrantesController],
   providers: [IntegrantesService],
 })
