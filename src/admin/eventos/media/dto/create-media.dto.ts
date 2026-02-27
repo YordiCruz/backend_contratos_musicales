ca3a2f0b21caae2f33210f30faa2a5fd40b72935
Yordi <yordi.cruz@gmail.com>
2026-02-27 01:32:07 -0400

    dto de media

--- src/admin/eventos/media/dto/create-media.dto.ts
+++ src/admin/eventos/media/dto/create-media.dto.ts
@@ -1,1 +1,26 @@
-export class CreateMediaDto {}
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, IsUUID } from "class-validator";
+
+export class CreateMediaDto {
+  @IsUUID()
+  @IsString()
+  @IsNotEmpty()
+  id_evento: string;
+
+  @IsString()
+  tipo: string; // imagen, video
+
+  @IsString()
+  url: string;
+
+  @IsOptional()
+  @IsString()
+  descripcion?: string;
+
+  @IsOptional()
+  orden?: number;
+
+  @IsOptional()
+  @IsBoolean()
+  visibilidad_publica?: boolean;
+
+}
